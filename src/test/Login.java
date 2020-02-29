494019c9df5135737c40ef94eb8670faf2880ff4
Enes Turan <trnns2015@gmail.com>
2020-02-29 12:40:09 -0500

    update from intelliJ

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,7 +3,9 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Hello Git, Marufjohn has a new baby. Today Asiya teachong us GITHUB and GIT. Hello Asiya");
-        System.out.println("God, please show me one way to make easier and better life. Givem ea power to strugle with this tough life.");
+
+
+        // this line from intelliJ
     }
 }
 
