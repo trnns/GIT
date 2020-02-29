58c2f854af7dd1e4331d3910a58a35836215be1b
Enes Turan <trnns2015@gmail.com>
2020-02-29 10:37:49 -0500

    add Login.java

---
+++ src/test/Login.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Hello Git, Marufjohn has a new baby. Today Asiya teachong us GITHUB and GIT. Hello Asiya");
+    }
+}
